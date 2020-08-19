14b12503193a9f8d6c6d6d92cade990f0c23aa36
代慶 真 <makoto-yokei@MacBook-Pro.local>
2020-08-19 18:24:57 +0900

    ユーザー登録/ログイン1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
