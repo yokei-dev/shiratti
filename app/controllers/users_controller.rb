40442d9b2c44d8a807e9c990b59beeaf36134b00
代慶 真 <makoto-yokei@MacBook-Pro.local>
2020-08-21 17:55:04 +0900

    タスク開始3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,16 @@ class UsersController < ApplicationController
   end
 
   def todo
-    @tasks = User.find_by(id: params[:id]).tasks.order('created_at DESC')
+    @tasks = User.find_by(id: params[:id]).tasks.where(status: 0).order('created_at DESC')
   end
 
   def doing
-    @daily_tasks = current_user.daily_tasks.where(add_date: Date.today.to_s)
+    @daily_tasks = User.find_by(id: params[:id]).daily_tasks.where(add_date: Date.today.to_s)
   end
 
   def done
     @user = User.find(params[:id])
-    @tasks = @user.tasks.where(status: 2).order('created_at DESC')
+    @tasks = @user.tasks.where(status: 1).order('created_at DESC')
   end
 
 end
