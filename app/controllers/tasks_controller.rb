20d4be5889c4278457fef5aa0e4664ac45d7ec6e
kenta.nakazaki <nakazaki.kenta@lmi.ne.jp>
2020-08-21 18:24:31 +0900

    pull

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,6 @@
 class TasksController < ApplicationController
 	before_action :authenticate_user!
 
-
-
 	def show
 	end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,17 @@ class UsersController < ApplicationController
     @projects = @user.joinings
   end
 
+  def todo
+    @tasks = User.find_by(id: params[:id]).tasks.where(status: 0).order('created_at DESC')
+  end  
+
   def doing
     @daily_tasks = current_user.daily_tasks.where(add_date: Date.today.to_s)
+  end
+
+  def done
+    @user = User.find(params[:id])
+    @tasks = @user.tasks.where(status: 1).order('created_at DESC')
   end
 
   def show
