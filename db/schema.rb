bcadae241f3040938af461ab65d7d6d023122cae
kenta.nakazaki <nakazaki.kenta@lmi.ne.jp>
2020-08-22 13:22:15 +0900

    pull

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_064344) do
+ActiveRecord::Schema.define(version: 2020_08_22_020728) do
 
   create_table "daily_tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2020_08_20_064344) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["task_id"], name: "index_daily_tasks_on_task_id"
     t.index ["user_id"], name: "index_daily_tasks_on_user_id"
+  end
+
+  create_table "daily_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.float "motivation"
+    t.bigint "user_id", null: false
+    t.date "add_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_daily_users_on_user_id"
   end
 
   create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -67,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_064344) do
 
   add_foreign_key "daily_tasks", "tasks"
   add_foreign_key "daily_tasks", "users"
+  add_foreign_key "daily_users", "users"
   add_foreign_key "projects", "users", column: "boss_id"
   add_foreign_key "tasks", "projects"
   add_foreign_key "tasks", "users"
