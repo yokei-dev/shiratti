9d40cd621651845c50ea1b2c955c856e094e0656
代慶 真 <makoto-yokei@MacBook-Pro.local>
2020-08-25 19:36:26 +0900

    負債完済

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   end  
 
   def doing
-    @tasks_and_daily_tasks = TaskCollection.new(User.find_by(params[:id]),[],[])
-
+    # binding.pry
+    @tasks_and_daily_tasks = TaskCollection.new(User.find(params[:id]),[],[])
+    # binding.pry
     # @daily_tasks = current_user.daily_tasks.where(add_date: Date.today)
 
     # @daily_tasks = current_user.daily_tasks.where(add_date: Date.today)
